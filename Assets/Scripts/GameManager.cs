58b5daa131a048e3836e9e8f6adaa7cee5bcd92c
Benedek Balint Toth <benedek.toth@bookrkids.hu>
2023-06-27 17:25:54 +0200

    upload 2023-06-27 17-25

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int scores = 0;
+
+    public GameObject colaPrefab;
+    public GameObject burgerPrefab;
+
+    public Vector3[] colaSpawnPlaces;
+    public Vector3[] burgerSpawnPlaces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
